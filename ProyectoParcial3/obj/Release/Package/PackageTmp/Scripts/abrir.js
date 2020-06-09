b0560078b759e39916216c6c869f5c78df9a8564
Graz <61188923+Graz150@users.noreply.github.com>
2020-06-09 00:06:13 -0500

    4th commit

---
+++ ProyectoParcial3/obj/Release/Package/PackageTmp/Scripts/abrir.js
@@ -0,0 +1,18 @@
+﻿$('.menu-bar').on('click', function () {
+    $('.sidebar').toggleClass('abrir');
+});
+/*
+$('.menu-bar').on('click', function () {
+    $('.contenido').toggleClass('abrircontenido');
+});
+*/
+
+    $('.contenidosd').on('click', function(e){
+        e.preventDefault();
+        var SubMenu=$(this).next('ul');
+        var iconBtn=$(this).children('.zmdi-caret-down');
+        if(SubMenu.hasClass('show-sideBar-SubMenu')){
+            iconBtn.removeClass('zmdi-hc-rotate-180');
+            SubMenu.removeClass('show-sideBar-SubMenu');
+        }
+    });
